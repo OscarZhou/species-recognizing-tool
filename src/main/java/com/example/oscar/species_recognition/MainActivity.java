684157bdfcee96966891be6541923ff65d156406
OscarZhou <OscarZhou@users.noreply.github.com>
2017-06-04 18:16:42 +1200

    store picture into gallery
    two improvements:
    1 after taking a picture, the picture will stop the window instead of changing camera mode
    2 store new generated pictures into system gallery

--- src/main/java/com/example/oscar/species_recognition/MainActivity.java
+++ src/main/java/com/example/oscar/species_recognition/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.oscar.species_recognition;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Environment;
@@ -24,6 +29,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -44,13 +50,13 @@ public class MainActivity extends Activity {
     public Camera.PictureCallback mPicture;
 
     private String selectedImagePath;
-    //private ImageView imgview;
+    private ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //imgview = (ImageView) findViewById(R.id.image_view);
+        imgView = (ImageView) findViewById(R.id.image_view);
 
 
         Log.d(TAG, "--------------------------------startup the application!");
@@ -67,21 +73,45 @@ public class MainActivity extends Activity {
                 public void onPictureTaken(byte[] data, Camera camera) {
 
                     File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+                    //ImageInfo pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+
+                    Log.d("SRT", "picture directory = "+pictureFile);
+                    //insertImage(pictureFile);
                     if (pictureFile == null) {
                         //Log.d(TAG, "Error creating media file, check storage permissions: " + e.getMessage());
                         return;
                     }
 
+
                     try {
+
                         FileOutputStream fos = new FileOutputStream(pictureFile);
+                        Log.d("SRT", "what is the data?  "+data);
                         fos.write(data);
                         fos.close();
+
+
                     } catch (FileNotFoundException e) {
                         Log.d(TAG, "File not found: " + e.getMessage());
                     } catch (IOException e) {
                         Log.d(TAG, "Error accessing file: " + e.getMessage());
                     }
-                    mCamera.startPreview();
+
+
+
+
+                    //imginfo.setTitle("IMG_"+timeStamp);
+
+                    String filePath = pictureFile.getPath();
+                    Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+
+                    Log.d("SRT", "imginfo.getBitmap() = " + bitmap);
+                    //imgView.setVisibility(View.VISIBLE);
+                    //imgView.setImageBitmap(bitmap);
+
+                    String url = insertImage(getContentResolver(), bitmap, null, null);
+                    Log.d("SRT", "the return value of insertimage is " + url);
+                    //mCamera.startPreview();
                 }
             };
 
@@ -141,17 +171,20 @@ public class MainActivity extends Activity {
         // Create a media file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         File mediaFile;
+        //ImageInfo imginfo = new ImageInfo();
         if (type == MEDIA_TYPE_IMAGE){
             mediaFile = new File(mediaStorageDir.getPath() + File.separator +
                     "IMG_"+ timeStamp + ".jpg");
+
         } else if(type == MEDIA_TYPE_VIDEO) {
             mediaFile = new File(mediaStorageDir.getPath() + File.separator +
                     "VID_"+ timeStamp + ".mp4");
         } else {
             return null;
         }
-
+        Log.d("SRT", "Successful to create an image");
         return mediaFile;
+        //return imginfo;
     }
 
     public void click_capture(View view) {
@@ -160,6 +193,7 @@ public class MainActivity extends Activity {
         {
             //take photo
             mCamera.takePicture(null, null, mPicture);
+
         }
     }
 
@@ -183,6 +217,101 @@ public class MainActivity extends Activity {
 
 
     }
-}
 
+    public static final String insertImage(ContentResolver cr,
+                                           Bitmap source,
+                                           String title,
+                                           String description) {
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, title);
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, title);
+        values.put(MediaStore.Images.Media.DESCRIPTION, description);
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        // Add the date meta data to ensure the image is added at the front of the gallery
+        values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis());
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+
+        Uri url = null;
+        String stringUrl = null;    /* value to be returned */
+        try {
+            url = cr.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            Log.d("SRT", "url = "+url);
+            Log.d("SRT", "source = "+source);
+            if (source != null) {
+                OutputStream imageOut = cr.openOutputStream(url);
+                try {
+                    source.compress(Bitmap.CompressFormat.JPEG, 50, imageOut);
+                } finally {
+                    Log.d("SRT", "imageOut.close()");
+                    imageOut.close();
+                }
+
+                long id = ContentUris.parseId(url);
+                Log.d("SRT", "id = "+id);
+                // Wait until MINI_KIND thumbnail is generated.
+                Bitmap miniThumb = MediaStore.Images.Thumbnails.getThumbnail(cr, id, MediaStore.Images.Thumbnails.MINI_KIND, null);
+                // This is for backward compatibility.
+                storeThumbnail(cr, miniThumb, id, 50F, 50F, MediaStore.Images.Thumbnails.MICRO_KIND);
+            } else {
+                cr.delete(url, null, null);
+                url = null;
+            }
+        } catch (Exception e) {
+            if (url != null) {
+                cr.delete(url, null, null);
+                url = null;
+            }
+        }
+
+        if (url != null) {
+            stringUrl = url.toString();
+        }
+
+        return stringUrl;
+    }
+
+
+    private static final Bitmap storeThumbnail(
+            ContentResolver cr,
+            Bitmap source,
+            long id,
+            float width,
+            float height,
+            int kind) {
+
+        // create the matrix to scale it
+        Matrix matrix = new Matrix();
+
+        float scaleX = width / source.getWidth();
+        float scaleY = height / source.getHeight();
+
+        matrix.setScale(scaleX, scaleY);
+
+        Bitmap thumb = Bitmap.createBitmap(source, 0, 0,
+                source.getWidth(),
+                source.getHeight(), matrix,
+                true
+        );
+
+        ContentValues values = new ContentValues(4);
+        values.put(MediaStore.Images.Thumbnails.KIND,kind);
+        values.put(MediaStore.Images.Thumbnails.IMAGE_ID,(int)id);
+        values.put(MediaStore.Images.Thumbnails.HEIGHT,thumb.getHeight());
+        values.put(MediaStore.Images.Thumbnails.WIDTH,thumb.getWidth());
+
+        Uri url = cr.insert(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, values);
+
+        try {
+            OutputStream thumbOut = cr.openOutputStream(url);
+            thumb.compress(Bitmap.CompressFormat.JPEG, 100, thumbOut);
+            thumbOut.close();
+            return thumb;
+        } catch (FileNotFoundException ex) {
+            return null;
+        } catch (IOException ex) {
+            return null;
+        }
+    }
+}
 
